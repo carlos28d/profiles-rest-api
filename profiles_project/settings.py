9a79e2c00d28fe04a000d159f8b7650147e103a7
Carlos Enrique Gutierrez Juarez <cgutierrez@hermescorp.io>
2021-06-03 14:52:38 -0500

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2n2zm+@m&2t3lszwqg3k1g3h#=^6l&7w63zdhb%2bj*x6w@&s('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
